cee20c82c499dd4db5118e2393a952adfc757082
Sou1Jkeee <1nfern0@list.ru>
2019-11-11 02:50:00 +0300

    Notepad v1.0

--- task.rb
+++ task.rb
@@ -1,6 +1,26 @@
+require 'date'
+
 class Task < Post
   def initialize
     super
     @due_date = Time.now
   end
+
+  def read_from_console
+    puts"Что надо сделать?"
+    @text = STDIN.gets.chomp
+
+    puts "Укажите дату дедлайна в формате ДД.ММ.ГГГГ, например 10.09.2019"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
+  end
+
+  def to_strings
+  time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+  deadline = "Крайний срок #{@due_date}"
+
+  return [deadline, @text, time_string]
+end
 end
